17d8f27710dd7b6ecb716c1989b5654676cc6c2f
jPedroRoch4 <jpedro.devops@gmail.com>
2023-06-13 18:50:26 -0300

    2023.06.12-2

--- src/main/java/com/jp/java/DemoAPI1/TrecoRepository.java
+++ src/main/java/com/jp/java/DemoAPI1/TrecoRepository.java
@@ -2,6 +2,6 @@ package com.jp.java.DemoAPI1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TrecoRepository extends JpaRepository< Treco , Long> {
+public interface TrecoRepository extends JpaRepository<Treco, Long> {
 
 }
